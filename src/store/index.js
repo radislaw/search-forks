0baa4e9c6d621b25965b004c2e4f9f038582e82a
Radislaw <radislaw22@gmial.com>
2022-04-14 01:05:40 +1000

    add bootstrap vue, basic layout

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    forks: []
   },
   getters: {
   },
   mutations: {
+    SET_FORKS (state, forks) {
+      state.forks = forks
+    }
   },
   actions: {
+    async getForks ({ commit }, params) {
+      const url = 'https://api.github.com/repos/vuejs/core/forks'
+      fetch(url)
+        .then(response => response.json())
+        .then(forks => {
+          commit('SET_FORKS', forks)
+        })
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container py-5">
+    <h1>Welcome!</h1>
+    <div class="form-group">
+      <b-form-input size="lg" placeholder="Search" />
+    </div>
+
+    <div class="overflow-auto">
+      <p class="mt-3">Current Page: {{ currentPage }}</p>
+
+      <b-table
+        striped hover
+        :items="tableData"
+        :per-page="perPage"
+        :current-page="currentPage"
+      ></b-table>
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="my-table"
+      ></b-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      perPage: 10,
+      currentPage: 1
+    }
+  },
+  computed: {
+    ...mapState(['forks']),
+    rows () {
+      return this.forks.length
+    },
+    tableData () {
+      // eslint-disable-next-line camelcase
+      return this.forks.map(({ full_name, owner, stargazers_count, url }) =>
+        ({ repository_name: full_name, owner: owner.login, stars: stargazers_count, url }))
+    }
+  },
+  methods: mapActions(['getForks']),
+  mounted () {
+    this.getForks()
   }
 }
 </script>
